6cf6bbe80da174f652a8a37ab3c708e12f86d7cd
Sudhanshu Ranjan <sudhanshu.ranjan@nobroker.in>
2026-07-06 22:41:34 +0530

    Add ic_logo vector asset and use it in NotificationBannerEngine

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -15,7 +15,7 @@ plugins {
 }
 
 group = "io.github.srjranjan"
-version = "1.0.6"
+version = "1.0.7"
 
 kotlin {
     val isAndroidOnly = !(findPublishingProperty("publishTarget") ?: "android").equals("all", ignoreCase = true)
