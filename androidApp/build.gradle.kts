8593f6ee714446db02d1bef34cfd7ca3eba44f19
Sudhanshu Ranjan <sudhanshu.ranjan@nobroker.in>
2026-07-13 02:35:48 +0530

    Bump version to 1.0.11

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.srj.notificationinspector"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.11"
     }
     packaging {
         resources {
